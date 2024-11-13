f8a58780a7416317f25b01f1c849c9f8cafcd7ee
Evan Hynes <evan.hynes@stellarfusion.io>
2024-11-13 16:45:49 +0000

    Build command
    - Get version from package json
    - Load all 3 modules
    - Read arguments from cli
    - Handle errors

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello world!');
+import { Command } from 'commander';
+
+import packageJson from '@/root/package.json';
+
+import { BACKUP_MODULE_METADATA, handleBackupAction } from './backup';
+import { DIRECTORY_MODULE_METADATA, handleDirectoryAction } from './directory';
+import { addModuleToProgram } from './module';
+import { MUSIC_MODULE_METADATA, handleMusicAction } from './music';
+
+const start = async () => {
+  try {
+    console.info('Starting CLI utils...');
+
+    const program = new Command('CLI Utils');
+    program.version(packageJson.version);
+
+    addModuleToProgram(program, BACKUP_MODULE_METADATA, handleBackupAction);
+    addModuleToProgram(program, DIRECTORY_MODULE_METADATA, handleDirectoryAction);
+    addModuleToProgram(program, MUSIC_MODULE_METADATA, handleMusicAction);
+
+    await program.parseAsync(Bun.argv);
+  } catch (err) {
+    console.error(`Unable to start CLI: ${err.message} - ${err.stack}`);
+  }
+};
+
+void start();
